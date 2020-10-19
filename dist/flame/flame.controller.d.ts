8f582dcc245f27e9bf4018e73fb13822ac2ab3c9
re <thinhhuynh3108@gmail.com>
2020-10-19 21:56:07 +0700

    update

--- dist/flame/flame.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { FlameService } from './flame.service';
-export declare class FlameController {
-    private readonly flameService;
-    constructor(flameService: FlameService);
-    getHello(): string;
-}
