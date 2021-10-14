b367665a6ba612dc46deeff3aed0e59666cc40d5
Thinh Huynh <thinhhuynh3108@gmail.com>
2021-10-14 21:21:09 +0700

    Config mail

--- src/gmail/gmail.service.ts
+++ src/gmail/gmail.service.ts
@@ -9,7 +9,22 @@ import { ConfigService } from 'src/config/config.service'
 
 @Injectable()
 export class GmailService {
-	transporter: Mail = createTransport(this.configService.gmailTransport)
+	transporter: Mail = createTransport({
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true,
+		auth: {
+			type: 'OAuth2',
+			user: 'thcshiepphuoc2020@gmail.com', // SENDER_EMAIL_ADDRESS,
+			clientId:
+				'806917942860-h08emp26re674449bhc6l350p05s21h1.apps.googleusercontent.com', // MAILING_SERVICE_CLIENT_ID
+			clientSecret: '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', //'MAILING_SERVICE_CLIENT_SECRET'
+			refreshToken:
+				'1//04WZ51TCvy8XmCgYIARAAGAQSNwF-L9Ir_asvdfwYGzTuvcO_qPRNrdwDRNqicgAqi6twEnkSARNsvhydRKBajze9dmZhPwnGIco', //
+			accessToken:
+				'ya29.a0ARrdaM-pYh47Gb-UdmKQDH5rqLxfxTUkkW4a9zj4-ZTQfxAakxxNTai1TGkJgVjaYg3qM60ZpKPrapmVTbBZ4PRnEUw5A6O4bvJZJqcfw-1WUPRRDwsUQDvpAox2QExLXsau489BtsUCZ9BKNIc6-EpNL5Nr', // ACCESS_SERVICE_TOKEN
+		},
+	})
 
 	constructor(
 		@InjectModel(GPS_MODEL)
